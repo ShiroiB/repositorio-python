4948706db008c6ae790cc24e3b404cf026db688a
Emily <69693425+ShiroiB@users.noreply.github.com>
2022-01-18 17:16:43 -0300

    Create 07-desconto.py

---
+++ 07-desconto.py
@@ -0,0 +1,9 @@
+n1 = float(input('digite o valor do produto: R$ '))
+desconto = n1 - (n1*0.05)
+print('o produto que custava R$ {}, com desconto de 5% ira custar 
+{:.2f}'.format(n1,desconto))
+ reajuste do salario
+n1 = float(input('digite o valor do salario: R$ '))
+p1 = n1 + (n1*0.15)
+print('o produto que custava R$ {}, com aumento de 15% o salario 
+ficara em {:.2f}'.format(n1,p1))
