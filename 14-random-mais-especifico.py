465ef7364e371b0173930c62f366e8d54d4c8b81
Emily <69693425+ShiroiB@users.noreply.github.com>
2022-01-18 18:06:30 -0300

    Update and rename 14-catetos-e-hipotenusa.py to 14-random-mais-especifico.py

---
+++ 14-random-mais-especifico.py
@@ -0,0 +1,9 @@
+
+import random
+aluno1 = str(input('digite um nome '))
+aluno2 = str(input('digite um nome '))
+aluno3 = str(input('digite um nome '))
+aluno4 = str(input('digite um nome '))
+d= [aluno1,aluno2,aluno3,aluno4]
+random.shuffle(d)
+print('o aluno sorteado e {}'.format(d)
