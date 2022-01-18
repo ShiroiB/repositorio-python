d5c392f10cf469d03945a8d4c2919f32e724208b
Emily <69693425+ShiroiB@users.noreply.github.com>
2022-01-18 17:36:49 -0300

    Create 11-seno-cosseno-tangente.py

---
+++ 11-seno-cosseno-tangente.py
@@ -0,0 +1,9 @@
+
+import math
+n1 = float(input('digite um angulo\n'))
+ang = math.radians(n1)
+cos1 = math.cos(ang)
+sin1 = math.sin(ang)
+tan1 = math.tan(ang)
+print('o cosseno e {:.2f}, seu seno e {:.2f} e o sua tangente e 
+{:.2f}'.format(cos1,sin1,tan1))
