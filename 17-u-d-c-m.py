cdfbc207dc97ee22647eed8ac1b077bfc479e739
Emily <69693425+ShiroiB@users.noreply.github.com>
2022-01-18 18:26:44 -0300

    Create 17-u-d-c-m.py

---
+++ 17-u-d-c-m.py
@@ -0,0 +1,5 @@
+n1 =str(input('digite um valor de 0 a 9999: '))
+print('milhares:{}'.format(n1[0]))
+print('centenas:{}'.format(n1[1]))
+print('dezenas:{}'.format(n1[2]))
+print('unidades:{}'.format(n1[3]))
