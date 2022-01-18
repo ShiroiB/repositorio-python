4b053bd685708a781c8d2fe18ca9282ab5b7ef1c
Emily <69693425+ShiroiB@users.noreply.github.com>
2022-01-18 17:08:56 -0300

    Create media.py

---
+++ media.py
@@ -0,0 +1,5 @@
+n1 = float(input('digite numero\n'))
+n2 = float(input('digite numero\n'))
+media = (n1+n2)/2 
+print('sua media e de {:.1f}, suas notas sao: primeira e {} e a segunda 
+e {}'.format(media, n1,n2))
