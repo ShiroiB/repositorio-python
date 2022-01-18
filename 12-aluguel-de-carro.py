f9a009b7c03dcc7fcd71708ed2c90705336309ba
Emily <69693425+ShiroiB@users.noreply.github.com>
2022-01-18 18:02:08 -0300

    Create 12-aluguel-de-carro.py

---
+++ 12-aluguel-de-carro.py
@@ -0,0 +1,7 @@
+
+n1 = float(input('quantos dias alugados: '))
+n2 = float(input('quantos km rodados: '))
+dia = n1*60
+km = n2*0.15
+soma = dia + km
+print('o total a pagar e de R$ {}'.format(soma))
