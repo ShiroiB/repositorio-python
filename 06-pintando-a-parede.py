eea5fa03fdc8574c86c6a46c6c3ada2c329cdb3c
Emily <69693425+ShiroiB@users.noreply.github.com>
2022-01-18 17:16:11 -0300

    Create 06-pintando-a-parede.py

---
+++ 06-pintando-a-parede.py
@@ -0,0 +1,7 @@
+n1 = float(input('digite a base: '))
+n2 = float(input('digite a altura: '))
+area = n1*n2
+litro = area/2
+print('a area da parede e de {} m²'.format(area))
+print ('a quantidade de tinta que deve ser usada e de 
+{}L'.format(litro))
