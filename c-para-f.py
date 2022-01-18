1f301f2f50d3d26b31d07158517e24018c5fbe18
Emily <69693425+ShiroiB@users.noreply.github.com>
2022-01-18 17:17:27 -0300

    Create c-para-f.py

---
+++ c-para-f.py
@@ -0,0 +1,10 @@
+n1 = float(input('digite a temperatura em °C: '))
+f = (n1*1.8)+32
+print ('a temperatura e equivalente a {}°F'.format(f))
+ aluguel de carro
+n1 = float(input('quantos dias alugados: '))
+n2 = float(input('quantos km rodados: '))
+dia = n1*60
+km = n2*0.15
+soma = dia + km
+print('o total a pagar e de R$ {}'.format(soma))
