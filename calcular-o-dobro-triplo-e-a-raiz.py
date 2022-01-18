94ec95f92df6b833840f7b690438e5a9475393c0
Emily <69693425+ShiroiB@users.noreply.github.com>
2022-01-18 17:08:16 -0300

    Create calcular-o-dobro-triplo-e-a-raiz.py

---
+++ calcular-o-dobro-triplo-e-a-raiz.py
@@ -0,0 +1,6 @@
+n1 = int(input('digite algo\n'))
+raizquadrada = n1**(1/2)
+dobro = n1*2
+triplo = n1*3
+print('o seu numero e {}, a raiz quadrada e {:.2f}, seu dobro e {}, seu 
+triplo e {}'.format(n1,raizquadrada,dobro,triplo)
