8fdf7605c3f354cd5e0740593533904d462086dc
Emily <69693425+ShiroiB@users.noreply.github.com>
2022-01-18 17:09:51 -0300

    Create conversor-de-medida-comprimento.py

---
+++ conversor-de-medida-comprimento.py
@@ -0,0 +1,14 @@
+n1 = float(input('digite numero em metro\n'))
+Quilômetro = n1/1000
+Hectometro = n1/100
+decametro = n1/10
+decimetro = n1*10
+centimetro = n1*100
+milimetro = n1*1000
+print('{}km'.format(Quilômetro))
+print('{}hm'.format(Hectometro))
+print('{}dam'.format(decametro))
+print('{}m'.format(n1))
+print('{}dm'.format(decimetro))
+print('{}cm'.format(centimetro))
+print('{}mm'.format(milimetro))
