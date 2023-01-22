f607ade6663eda1bf0785e06c4f28c903a657290
Emily <69693425+ShiroiB@users.noreply.github.com>
2023-01-22 16:46:34 -0300

    Create 32-comparacao_num.py

---
+++ 32-comparacao_num.py
@@ -0,0 +1,8 @@
+n1 = float(input('digite um numero: '))
+n2 = float(input('digite outro numero: '))
+if n1 >n2:
+   print(' o primerio e maior')
+elif n1==n2:
+   print(' os dois são iguais')
+else:
+   print ('o segundo e maior')
